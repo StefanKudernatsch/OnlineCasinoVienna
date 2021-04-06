fa2a58d557bdb52ab02e7f9add711289050ddfd3
Leon von Voss <if20b169@technikum-wien.at>
2021-04-06 18:05:15 +0200

    Added new SQL-Dump
    Edited Database-.Dump

--- onlinecasino.sql
+++ onlinecasino.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 05. Apr 2021 um 21:01
+-- Erstellungszeit: 06. Apr 2021 um 18:02
 -- Server-Version: 10.4.14-MariaDB
 -- PHP-Version: 7.4.10
 
@@ -35,6 +35,14 @@ CREATE TABLE `history` (
   `PlayTime` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Daten für Tabelle `history`
+--
+
+INSERT INTO `history` (`ID`, `TotalGames`, `PlayTime`) VALUES
+(0, 0, '00:00:00'),
+(1, 0, '00:00:00');
+
 -- --------------------------------------------------------
 
 --
@@ -46,18 +54,26 @@ CREATE TABLE `user` (
   `FirstName` varchar(50) NOT NULL,
   `LastName` varchar(50) NOT NULL,
   `Gender` varchar(50) NOT NULL,
+  `Birthday` date NOT NULL,
   `UserName` varchar(50) NOT NULL,
   `Email` varchar(100) NOT NULL,
   `Password` varchar(50) NOT NULL,
-  `UserImage` varchar(200),
+  `UserImage` varchar(200) NOT NULL,
   `Money` float NOT NULL,
   `Adress` varchar(100) NOT NULL,
-  `Post` int(4) NOT NULL,
+  `PostalCode` int(4) NOT NULL,
   `City` varchar(50) NOT NULL,
   `Active` tinyint(1) NOT NULL,
   `Banned` tinyint(1) NOT NULL,
   `HistoryID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `user`
+--
+
+INSERT INTO `user` (`ID`, `FirstName`, `LastName`, `Gender`, `Birthday`, `UserName`, `Email`, `Password`, `UserImage`, `Money`, `Adress`, `PostalCode`, `City`, `Active`, `Banned`, `HistoryID`) VALUES
+(1, 'Max', 'Mustermann', 'männlich', '2021-04-06', 'Maxl', 'maxmustermann@email.com', 'Max', '', 0, 'Musterstraße 122', 1010, 'Wien', 1, 0, 1);
 
 --
 -- Indizes der exportierten Tabellen
@@ -84,7 +100,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT für Tabelle `user`
 --
 ALTER TABLE `user`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints der exportierten Tabellen
